922d70005d4c12466230dbc46462c62e68d3ae27
StellarH <107297301+StellarH@users.noreply.github.com>
2023-01-19 00:05:09 +0800

    Build script for GCC on Windows with MinGW-w64

---
+++ build-project-GCC.cmd
@@ -0,0 +1,20 @@
+@echo off
+
+IF NOT EXIST ".\build-project" (
+
+	MKDIR ".\build-project"
+)
+
+CD /d ".\build-project"
+
+cmake -S ..\ -B . -G "MinGW Makefiles"
+
+IF EXIST ".\Makefile" (
+	make help
+	make TDOM-EncryptOrDecryptFile-Reborn
+) ELSE (
+	echo ==============================================================================
+	echo CMake Failed
+)
+
+PAUSE
