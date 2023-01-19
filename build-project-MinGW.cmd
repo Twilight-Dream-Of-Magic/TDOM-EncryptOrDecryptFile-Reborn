12c6c77e03a93e6dcbcea0670d7ae6edaa1dd243
StellarH <107297301+StellarH@users.noreply.github.com>
2023-01-19 16:18:55 +0800

    Update build-project-MinGW.cmd

--- build-project-MinGW.cmd
+++ build-project-MinGW.cmd
@@ -11,8 +11,8 @@ cmake -S .. -B . -G "MinGW Makefiles"
 IF EXIST "Makefile" (
 	make TDOM-EncryptOrDecryptFile-Reborn
 ) ELSE (
-	echo ==============================================================================
-	echo CMake Failed
+	ECHO ==============================================================================
+	ECHO CMake Failed
 )
 
 PAUSE
